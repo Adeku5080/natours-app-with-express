292a6823c7c2c9f0fae8472d55bc993ed05b0949
Adeku Ali <Aliadeku.aam@gmail.com>
2022-09-23 22:36:48 +0100

    create endpoint to get a single tour

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
-const express = require('express')
-
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
-const tours = readFileSync('/')
+// middleware to modify incoming data
+app.use(express.json());
 
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
+// get all tours
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours,
+    },
+  });
+});
 
+// get a single tour
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
 
+  const tour = tours.find((tour) => id == tour.id);
 
+  res.status(200).json({
+    status :'success',
+    data :{
+      tour
+    }
+  })
+});
 
+app.listen(3001, () => {
+  console.log("server is running ");
+});
 
+//create a tour
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = {
+    ...req.body,
+    id: newId,
+  };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
-
-
-app.listen(3001,()=>{
-    console.log('server open')
-})
 
