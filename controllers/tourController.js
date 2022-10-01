33b9c43fc64234518bd61f89f35c753d26ccb7c6
Adeku Ali <Aliadeku.aam@gmail.com>
2022-10-01 01:13:08 +0100

    get a tour,update and delete using the Tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -20,12 +20,13 @@ exports.createATour = async (req, res) => {
 exports.getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find();
-    res.status(201).json({
+
+    console.log(tours, "Ali");
+    res.status(200).json({
       status: "success",
       data: {
         tours,
       },
-      message: "data fetched successfully",
     });
   } catch (err) {
     res.status(404).json({
@@ -35,10 +36,57 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getATour = (req, res) => {
-  const id = req.params.id * 1;
+exports.getATour = async (req, res) => {
+  try {
+    // const iD = req.params.id;
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: "succesful",
+      data: {
+        tour,
+      },
+    });
+  }catch (err) {
+     res.status(404).json({
+      status : 'fail',
+      message :err
+     })
+  }
 };
 
-exports.updateATour = (req, res) => {};
+exports.updateATour = async (req, res) => {
+  try{
+   const tour =  await Tour.findByIdAndUpdate(req.params.id,req.body,{
+      new : true
+    })
+     res.status(200).json({
+      status: "succesful",
+      data: {
+        tour
+      }
+    })
+  
+}catch(err){
+    res.status(404).json({
+      status : 'fail',
+      message :err
+     })
+}
+}
 
-exports.deleteATour = (req, res) => {};
+
+exports.deleteATour = async (req, res) => {
+  try{
+    await Tour.findByIdAndDelete(req.params.id)
+     res.status(204).json({
+       status: "succesful"
+       
+     });
+
+  }catch{
+     res.status(404).json({
+       status: "fail",
+       message: err,
+     });
+  }
+};
