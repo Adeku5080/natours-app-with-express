d7b3b5fba09b7c9df2e9d91303f851f9077715dc
Adeku Ali <Aliadeku.aam@gmail.com>
2022-10-07 17:15:52 +0100

    Add JWT for authentication on sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,21 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken")
 
 exports.signup = async (req, res, next) => {
-  //   if (req.body.password !== req.body.passwordConfirm) {
-  //     return res.status(401).json({ status: "passwords do not match" });
-  //   }
-  try {
-    const newUser = await User.create(req.body);
 
+  try {
+    const newUser = await User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        passwordConfirm:req.body.passwordConfirm
+    });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION,    
+    });
     res.status(201).json({
       status: "success",
+      token,
       data: {
         user: newUser,
       },
